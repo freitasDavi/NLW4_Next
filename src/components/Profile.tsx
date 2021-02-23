d166ceb1e1cc436e2a75794446556c905f78e1e7
Davi Freitas <davi.frrs@outlook.com>
2021-02-23 13:23:16 -0300

    Commit Inicial - aula2/50:11

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile () {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/63361459?s=460&u=33e80a9511b33e04d6b9da609fe0f3314f47c1c9&v=4" alt="Davi Freitas"/>
+            <div>
+                <strong>Davi Freitas</strong>
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    Level 1
+                </p>
+            </div>
+
+        </div>
+    );
+}
