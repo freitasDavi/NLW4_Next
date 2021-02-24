87b075d553373685e4d7807704d0d3834d675b3f
Davi Freitas <davi.frrs@outlook.com>
2021-02-24 13:22:40 -0300

    Countdown component and hook 13:39

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import ExperienceBar from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
+import { Countdown } from '../components/Countdown';
 import styles from '../styles/pages/Home.module.css';
+
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
@@ -19,3 +27,4 @@ export default function Home() {
   )
 }
  
+// jornadainfinita
